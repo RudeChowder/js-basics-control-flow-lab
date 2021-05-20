477bdcae742bcc6b688da0a06b590a00c6fd5f9e
Stephen Crane <scrane88@gmail.com>
2021-05-20 18:17:20 -0400

    done

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  let result;
+  if (feet <= 200){
+     result = "This one is on me!";
+  } else if (feet > 2000 && feet <= 2500) {
+    result = "I will gladly take your thirty bucks."
+  } else if (feet > 2500) {
+    result = "No can do."
+  }
+  return result;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  const result = (city === "NYC") ? "Ok, sounds good." : "No go.";
+  return result;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+      break;
+    case 'not as generous':
+      return "Thank you.";
+      break;
+    default:
+      return "Bye.";
+      break;
+  }
 }
