64874ef220963f0b26f80b2dc74e04926a00af8d
Stephen Crane <scrane88@gmail.com>
2021-05-21 11:40:33 -0400

    fixed first function

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function scuberGreetingForFeet(feet){
   let result;
-  if (feet <= 200){
+  if (feet <= 400){
      result = "This one is on me!";
   } else if (feet > 2000 && feet <= 2500) {
     result = "I will gladly take your thirty bucks."
